ec13e9c7fa7127ee9af1326d89239529e382206d
박상후 <sksk8922@gmail.com>
2023-07-10 18:46:58 +0900

    [신규] categoryController createCategory 메소드 테스트 코드 구현

--- test/Unit/Category/categoryController.test.ts
+++ test/Unit/Category/categoryController.test.ts
@@ -1,26 +1,28 @@
-import { instance, mock, when } from "ts-mockito";
+import { deepEqual, instance, mock, verify, when } from "ts-mockito";
 import { CategoryController } from "../../../src/Category/CategoryController"
 import { CategoryService } from "../../../src/Category/CategoryService";
 import { CategoryDto } from "../../../src/Category/dto/CategoryDto";
 import { Category } from "../../../src/Category/Category";
+import exp = require("constants");
 
 
 describe('categoryController test', () => {
     let categoryController:CategoryController;
     let mockedService:CategoryService;
-    let categoryDto:CategoryDto;
-    let category:Category;
+    
     const now = new Date();
+    
+    let categoryDto:CategoryDto;
+    categoryDto = new CategoryDto();
+    categoryDto.name = 'test name'
+    
+    let category:Category;
+    category = categoryDto.toEntity(now);
+    category.id = 1
 
     beforeEach( () => {
         mockedService = mock(CategoryService)
         categoryController = new CategoryController(instance(mockedService))
-        categoryDto = new CategoryDto();
-        categoryDto.name = 'test name'
-
-        category = categoryDto.toEntity(now);
-        category.id = 1
-
     })
 
     describe('createCategory test', () => {
@@ -30,7 +32,12 @@ describe('categoryController test', () => {
         } )
 
         it('should return category', async () => {
-            when(mockedService.createCategory(categoryDto)).thenResolve(category)
+            when(mockedService.createCategory(deepEqual(categoryDto))).thenResolve(category)
+
+            const result = await categoryController.createCategory(categoryDto);
+
+            verify(mockedService.createCategory(deepEqual(categoryDto))).once()
+            expect(result).toBe(category)
         })
 
     })
